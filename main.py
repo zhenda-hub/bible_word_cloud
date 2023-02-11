1923fcb9b5d8860b99dd48925349b431c1e428db
zzdnb <1955093734@qq.com>
2023-02-11 21:55:19 +0800

    加pyecahrt版本

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 # Author: zhenda
 from src.gen_word_cloud import *
+from src.gen_word_cloud_echarts import gene_wc_echarts, dict2list
 from src.utils.gene_fold import gene_fold
 from src.utils.mylog import setlogging
 
@@ -21,9 +22,28 @@ def gene_hgb(num):
         draw_word_img(res_dict, f'out/hgb_wordcloud{i}.png')
 
 
+def gene_bbe_echarts():
+    words_list = read_words_bbe(file='resources/bbe.txt')
+    stop_words = read_stop_words(file='resources/stop_words.txt')
+    res_dict = get_res_dict(words_list, stop_words)
+    res_lst = dict2list(res_dict)
+    gene_wc_echarts(res_lst, f'out/bbe_wordcloud.html')
+
+
+def gene_hgb_echarts():
+    words_list = read_words_hgb(file='resources/hgb.txt')
+    stop_words = read_stop_words(file='resources/stop_words.txt')
+    res_dict = get_res_dict(words_list, stop_words)
+    res_lst = dict2list(res_dict)
+    gene_wc_echarts(res_lst, f'out/hgb_wordcloud.html')
+
+
 if __name__ == '__main__':
     gene_fold('out')
     setlogging('out/main.log')
 
     # gene_bbe(2)
-    gene_hgb(2)
+    # gene_hgb(2)
+
+    gene_bbe_echarts()
+    gene_hgb_echarts()
--- tests/test_gwc.py
+++ tests/test_gwc.py
@@ -1,7 +1,12 @@
 # import sys
-# print(sys.path)
+# import pprint
+# pprint.pprint(sys.path)
+
+
 import unittest
+import pytest
 from src import gen_word_cloud
+from src.gen_word_cloud_echarts import gene_wc_echarts
 from src.utils.gene_fold import gene_fold
 import os
 from src.utils.mylog import setlogging
@@ -22,11 +27,6 @@ class TestWc(unittest.TestCase):
         words_gene = gen_word_cloud.read_words_bbe(file='resources/sub_bbe.txt')
         self.assertTrue(words_gene)
 
-    def test_read_words(self):
-        words_gene = gen_word_cloud.read_words(file='resources/sub_bbe.txt')
-        self.assertTrue(words_gene)
-
-
     def test_read_stop_words(self):
         stop_words = gen_word_cloud.read_stop_words(file='resources/stop_words.txt')
         self.assertIsInstance(stop_words, list)
@@ -46,9 +46,26 @@ class TestWc(unittest.TestCase):
         }
         addr = gen_word_cloud.draw_word_img(res_dict, f'{self.out_path}/demo1.png')
         self.assertTrue(os.path.exists(addr))
+        
+    # @pytest.mark.skip
+    def test_gene_wc_echarts(self):
+        res_dict = [
+            ('a', 2),
+            ('b', 2),
+            ('c', 10),
+            ('d', 4),
+            ('e', 14),
+            ('f', 114),
+            ('g', 43),
+            ('h', 4),
+            ('i', 24),
+        ]
+        addr = gene_wc_echarts(res_dict, f'{self.out_path}/demo1.html')
+        self.assertTrue(os.path.exists(addr))
 
 
 if __name__ == '__main__':
     gene_fold('out')
     setlogging('out/testWc.log')
+    
     unittest.main()
